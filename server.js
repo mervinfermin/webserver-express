feccbd1c0e9a85500dccfada9d711f0c6c250ed9
Mervin <mervin_fermin@hotmail.com>
2022-03-23 11:03:59 -0400

    segundo commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const hbs = require('hbs');
 //require('./hbs/helpers');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'))
 
@@ -33,5 +33,5 @@ app.get('/about', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log('escuchando peticiones en el puerto' + port);
+    console.log(`escuchando peticiones en el puerto' ${port} `);
 });
